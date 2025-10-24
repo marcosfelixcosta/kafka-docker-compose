0486c972c904276aeaeb3edd63137ff18eb3d18e
Marcos Felix <marcosfelixcosta@gmail.com>
2025-10-24 19:43:47 -0300

    first commit

---
+++ str_produce/src/main/java/com/marcosfelix/str_produce/StrProduceApplication.java
@@ -0,0 +1,13 @@
+package com.marcosfelix.str_produce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProduceApplication.class, args);
+	}
+
+}
---
+++ str_produce/src/test/java/com/marcosfelix/str_produce/StrProduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosfelix.str_produce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
